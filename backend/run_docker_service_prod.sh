7e40f000f5ad191ea03146a9409214d7762e3bb5
Alain Nambii <alainnambi@gmail.com>
2025-08-18 08:53:17 +0200

    feat: enhance Docker service script with improved output and background execution

--- backend/run_docker_service_prod.sh
+++ backend/run_docker_service_prod.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Script to run Docker environment and execute tests
+# 🚀 Script to run Docker environment and execute tests
 
-echo "Starting Docker environment..."
-docker compose -f docker-compose.prod.yml up --build
+echo "🐳 Starting Docker environment..."
+docker compose -f docker-compose.prod.yml up --build -d
+
+echo "✅ Docker environment started!"
+
+echo "🌱 Running seed to populate the database..."
+docker compose -f docker-compose.prod.yml exec web python manage.py seed
+
+echo "🎉 Done!"
